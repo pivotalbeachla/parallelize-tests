9ac9f6bfc4aab54db2edd88e323b642efc79b9c5
Your Name <you@example.com>
2016-10-06 11:46:51 -0700

    Update with better reporting of failures

--- run.rb
+++ run.rb
@@ -25,23 +25,23 @@ require 'sshkit/dsl'
 include SSHKit::DSL
 
 tests_results = []
-failed = false
 
 puts "### Running tests"
 
-on %w{parallel-ios-testbox-1.local parallel-ios-testbox-2.local }, in: :parallel, wait: 5 do |host|
+on %w{parallel-ios-testbox-1.local parallel-ios-testbox-2.local parallel-ios-testbox-3.local }, in: :parallel, wait: 5 do |host|
   as :pivotal  do
     within "workspace/parallelize-tests" do
       capture(:git, "pull")
+      capture(:xcodebuild, "-scheme UnitTest build-for-testing -destination \"platform=iOS Simulator,name=iPhone 7\"")
       while !list_of_tests.empty? do
         # TODO: check if it's thread safe
         if nextTest = list_of_tests.pop
           begin
-            test_run_result = capture(:xcodebuild, "-scheme UnitTest test -destination \"platform=iOS Simulator,name=iPhone 7\" -only-testing:#{nextTest}")
+            test_run_result = capture(:xcodebuild, "-scheme UnitTest test-without-building -destination \"platform=iOS Simulator,name=iPhone 7\" -only-testing:#{nextTest}")
             tests_results << { name: nextTest, success: true, output: test_run_result }
-          rescue
+          rescue Exception => e
             failed = true
-            tests_results << { name: nextTest, success: false, output: test_run_result }
+            tests_results << { name: nextTest, success: false, output: e.to_s }
           end
         end
       end
@@ -49,9 +49,12 @@ on %w{parallel-ios-testbox-1.local parallel-ios-testbox-2.local }, in: :parallel
   end
 end
 
-tests_results.each {|t| puts t[:output] unless t[:success] }
-
-if failed
-  puts "FAIL"
+failing_tests = tests_results.inject([]) do |arr, result|
+  if !result[:success]
+    arr += result[:output][/Failing tests:\s+(.+)\n\*\*/].scan(/(-\[.+\])/).flatten
+  end
 end
 
+unless failing_tests.empty?
+  puts "\nFailing tests:\n#{failing_tests.join("\n")}"
+end
